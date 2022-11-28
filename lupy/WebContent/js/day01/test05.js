3e812b419e7131d89b2218af12c09fb009ae49e2
yte8760 <118231976+yte8760@users.noreply.github.com>
2022-11-28 15:47:16 +0900

    ㅁ

--- lupy/WebContent/js/day01/test05.js
+++
@@ -1,1 +0,0 @@
-document.write('<h6 style="text-align: center, color: dodgerblue;">스크립트에서 추가한 태그 </h6>')
--- lupy/WebContent/js/day01/test06.js
+++
@@ -1,4 +0,0 @@
-var num;
-var obj = null;
-document.getElementById('txt').innerHTML = typeof 100 + '<br>';
-// document.getElementById('txt').innerHTML = typeof 100 + '<br>';
